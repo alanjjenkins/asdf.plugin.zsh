60c49987ec42a04bf4f9c7452ba4ce9b0a445b08
Alan Jenkins <a.jenkins@synalogik.co.uk>
2021-02-03 19:10:13 +0000

    Make the plugin automatically install asdf if it is not installed

--- asdf.plugin.zsh
+++ asdf.plugin.zsh
@@ -1,5 +1,7 @@
 asdf_dir="${asdf_dir:-$HOME/.asdf}"
 
+test ! -d "$asdf_dir" && git clone git@github.com:asdf-vm/asdf.git "$asdf_dir"
+
 if [[ -d $asdf_dir ]]; then
   source $asdf_dir/asdf.sh
   source $asdf_dir/completions/asdf.bash
